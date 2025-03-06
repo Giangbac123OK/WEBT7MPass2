4ef8ddfdbbbaf07bb35c8cf036a6b2c47fd11c82
PC <thanhanh20044@gmail.com>
2025-03-06 22:19:12 +0700

    thay dooir trang chu trang home

--- New folder/app.js
+++ New folder/app.js
@@ -3,7 +3,7 @@ const app = angular.module("myApp",['ngRoute']);
 app.config(function($routeProvider){
     $routeProvider
         .when('/',{
-            templateUrl: './Views/home.html'
+            templateUrl: '/WEBT7MPass2/New folder/Views/home.html'
         })
         .when('/giang',{
              template: '<h1>Giang</h1>'
