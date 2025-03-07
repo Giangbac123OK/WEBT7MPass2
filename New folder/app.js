294d7fe2e7e7a13bc207c4dbb4def4e4a0411220
HoàngThanh Giang <hoangthanhgiang22@gmail.com>
2025-03-07 08:52:44 +0700

    giangok

--- New folder/app.js
+++ New folder/app.js
@@ -5,8 +5,5 @@ app.config(function($routeProvider){
         .when('/',{
             templateUrl: './Views/home.html'
         })
-        .when('/giang',{
-             template: '<h1>Giang</h1>'
-         })
         .otherwise('/')
 })
