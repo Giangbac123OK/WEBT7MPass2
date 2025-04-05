68b363c243b1454a867ed24d24c432287f1f80e1
HoàngThanh Giang <hoangthanhgiang22@gmail.com>
2025-04-06 00:51:33 +0700

    ok

--- TemplateCustomer/Controller/donhangcuabanController.js
+++ TemplateCustomer/Controller/donhangcuabanController.js
@@ -657,7 +657,7 @@ $scope.init = function () {
                         }
                     });
                 };
-                
-    
+    // $http.get("https://localhost:7196/api/Trahangs")
+    //     .then()
 });
 
